43de4114dcc67fa5a1866161cec925665861b420
Simon Schaefer <saspeed@gmail.com>
2016-11-08 11:07:00 +0100

    Add Playing around with wpf

---
+++ ch.hsr.wpf.gadgeothek.main/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ch.hsr.wpf.gadgeothek.main
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public String ServerUrl { get; set; } = "http://localhost:8080";
+
+        public MainWindow()
+        {  
+            InitializeComponent();
+        }
+
+        private void AboutUsMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            
+            MessageBox.Show("Gadgeothek Admin Tool\n See: https://github.com/janicmikes/WPFTestat for more Info", "About",MessageBoxButton.OK,MessageBoxImage.Information);
+        }
+
+        private void ConnectToServerMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            ServerConnectDialog serverConnectDialog = new ServerConnectDialog();
+            if (serverConnectDialog.ShowDialog() == true)
+                this.ServerUrl = serverConnectDialog.Answer; 
+            StatusBarServerUrl.Text = ServerUrl; // Workaround, missing data binding
+        }
+      
+    }
+
+}
---
+++ ch.hsr.wpf.gadgeothek.main/ServerConnectDialog.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ch.hsr.wpf.gadgeothek.main
+{
+    /// <summary>
+    /// Interaction logic for ServerConnectDialog.xaml
+    /// </summary>
+    public partial class ServerConnectDialog : Window
+    {
+        String ServerUrl;
+
+        public ServerConnectDialog()
+        {
+            InitializeComponent();
+        }
+
+        public string Answer
+        {
+            get { return TextBoxServerUrl.Text; }
+        }
+
+        private void ButtonConnect_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+    }
+}
