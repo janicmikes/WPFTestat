ef9d2bab15e7622e0025ac808d008884b5bdbe14
Janic Mikes <janic.mikes@gmail.com>
2016-12-06 20:57:50 +0100

    added a placeholder window with all elements but not aranged

---
+++ ch.hsr.wpf.gadgeothek.main/SingleGadgetWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ch.hsr.wpf.gadgeothek.main
+{
+    /// <summary>
+    /// Interaction logic for SingleGadgetWindow.xaml
+    /// </summary>
+    public partial class SingleGadgetWindow : Window
+    {
+        public SingleGadgetWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
